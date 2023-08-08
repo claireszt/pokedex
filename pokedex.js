3729f069560a214d99aee1519aafc1de02b546c4
Claire <claireszt@gmail.com>
2023-08-08 16:49:53 +0200

    filled info bottom sheet

--- pokedex.js
+++ pokedex.js
@@ -1,27 +1,28 @@
+
 const pokemons_number = 151;
 
 const colors = {
-    normal: '#F5F5F5',
-    fighting: '#E6E0D4',
-    flying: '#F5F5F5',
-    poison: '#98d7a5',
-    ground: '#f4e7da',
-	rock: '#d5d5d4',
-    bug: '#f8d5a3',
-    ghost: '#ffffff',
-    steel: '#ffffff',
-	fire: '#FDDFDF',
-	water: '#DEF3FD',
-	grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	psychic: '#eaeda1',
-    ice: '#ffffff',
-	dragon: '#97b3e6',
-    dark: '#ffffff',
-	fairy: '#fceaff',
+	fire: '#fc7030',
+	grass: '#7ccc54',
+	electric: '#fcd434',
+	water: '#6c94f4',
+	ground: '#e4c46c',
+	rock: '#bca43c',
+	fairy: '#80746a',
+	poison: '#a444a4',
+	bug: '#acbc24',
+	dragon: '#743cfc',
+	psychic: '#ff84c4',
+	flying: '#ac94f4',
+	fighting: '#c4342c',
+	normal: '#80746a',
+	dark: '#745c4c',
+	ghost: '#784cd0',
+	ice: '#9cdcdc',
+	steel: '#bcbcd4'
 };
-const main_types = Object.keys(colors);
 
+const main_types = Object.keys(colors);
 
 const fetchPokemons = async () => {
 	for (let i = 1; i <= pokemons_number; i++) {
@@ -33,27 +34,166 @@ const getPokemon = async id => {
 	const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 	const res = await fetch(url)
 	const pokemon = await res.json()
+    createPokemonCard(pokemon)
+}
 
-    const pokemonTypes = []
+const createPokemonCard = (pokemon) => {
+	const pokemonEl = document.createElement('div');
+	pokemonEl.classList.add('pokemon');
+
+	const pokemonTypes = []
 
     for (let i = 0 ; i < pokemon.types.length ; i++) {
         pokemonTypes.push(pokemon.types[i].type.name)
     }
 
-    let type1 = pokemonTypes[0]
+    let type1 = `<div class="type">${pokemonTypes[0]}</div>`
     let type2 = ''
-    if (pokemonTypes[1] !== undefined && pokemonTypes[1] !== 'fairy') {
-        type2 = `<img id="label" src="img/label-pixel/${pokemonTypes[1]}.png">`
+    if (pokemonTypes[1] !== undefined) {
+        type2 = `<div class="type">${pokemonTypes[1]}</div>`
     }
 
-    document.querySelector('.pokedexContainer').innerHTML += `
-        <div class="pokemonCard">
-            <img src="${pokemon.sprites.front_default}">
-            <p>#${pokemon.id.toString().padStart(3,"0")}</p>
-            <h3>${pokemon.name}</h3>
-            <span><img id="label" src="img/label-pixel/${type1}.png"> ${type2}</span>
-        </div>
-    `
+	const type = main_types.find(type => pokemonTypes[0].indexOf(type) > -1);
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+	const color = colors[type];
+	
+	pokemonEl.style.backgroundColor = `${color}70`;
+
+	const pokeInnerHTML = `
+		<a href="pokedex.html?id=${pokemon.id}">
+			<div class="info">
+				<span class="number">#${pokemon.id
+								.toString()
+								.padStart(3, '0')}</span>
+				<h3 class="name" style="color:${color}">${name}</h3>
+				<div id="allTypes">${type1} ${type2}</div>
+			</div>
+			<div class="img-container">
+				<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png" alt="${name}" />
+				<div class="favorite"><i class="fa-regular fa-heart" style="color: #ffffff;"></i></div>
+			</div>
+		</a>
+			`;
+
+	pokemonEl.innerHTML = pokeInnerHTML;
+
+	document.querySelector('.poke-container').appendChild(pokemonEl);
+}
+
+const selectedPoke = location.href.split("=")[1]
+
+const fetchPokeData = async (id) => {
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+	const res = await fetch(url)
+	const pokemon = await res.json()
+	createPokeFiche(pokemon)
+	createBottomSheet(pokemon)
 };
 
-fetchPokemons()
+const createPokeFiche = (pokemon) => {
+	const ficheContainer = document.querySelector('.fiche-container')
+
+	const pokemonTypes = []
+
+    for (let i = 0 ; i < pokemon.types.length ; i++) {
+        pokemonTypes.push(pokemon.types[i].type.name)
+    }
+
+	const type1Index = main_types.find(type => pokemonTypes[0].indexOf(type) > -1);
+	const color1 = colors[type1Index];
+	let color2
+
+	if (pokemonTypes[1] !== undefined) {
+		const type2Index = main_types.find(type => pokemonTypes[1].indexOf(type) > -1);
+		color2 = colors[type2Index];
+	}
+
+	let type1 = `<div style="background-color:${color1}">${pokemonTypes[0]}</p></div>`
+    let type2 = ''
+    if (pokemonTypes[1] !== undefined) {
+        type2 = `<div style="background-color:${color2}">${pokemonTypes[1]}</div>`
+    }
+	
+	document.body.style.backgroundColor = `${color1}70`;
+
+	const ficheInnerHTML = `
+		<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png" alt="${name}" />
+		<h1 style="text-transform:capitalize">${pokemon.name}</h1>
+		<div class="types-fiche">${type1} ${type2}</div>
+	`
+
+	ficheContainer.innerHTML = ficheInnerHTML
+}
+
+async function fetchSpecies(selectedPoke) {
+	const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${selectedPoke}`);
+	const data = await response.json();
+	const species = data.genera[7].genus;
+	return species;
+  }
+
+
+const createBottomSheet = (pokemon) => {
+	const bottomSheet = document.createElement('div');
+
+	//info
+	bottomSheet.classList.add('info-bottom');
+	let pokeWeight 
+		if (pokemon.weight.toString().length == 1) {
+			pokeWeight = `0.${pokemon.weight} kg`
+		}
+		else {
+			pokeWeight = pokemon.weight.toString().slice(0, pokemon.weight.toString().length-1) + '.' + pokemon.weight.toString().slice(-1) + ' kg'
+		}
+
+	let pokeHeight 
+		if (pokemon.height.toString().length == 1) {
+			pokeHeight = `0.${pokemon.height} m`
+		}
+		else {
+			pokeHeight = pokemon.height.toString().slice(0, pokemon.height.toString().length-1) + '.' + pokemon.height.toString().slice(-1) + ' m'
+		}
+	
+	const abilities = pokemon.abilities.map((ability) => ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1))
+
+	fetchSpecies(pokemon.id)
+		.then((species) => {
+			const infoInnerHTML = `
+	<div id="title"><h1>info</h1> <i class="fa-solid fa-circle-info" style="color: #000000;"></i></div>
+	<table>
+		<tr>
+			<td class="table-title">Species<td>
+			<td class="table-data">${species}</td>
+		</tr>
+		<tr>
+			<td class="table-title">Weight<td>
+			<td class="table-data">${pokeWeight}</td>
+		</tr>
+		<tr>
+			<td class="table-title">Height<td>
+			<td class="table-data">${pokeHeight}</td>
+		</tr>
+		<tr>
+			<td class="table-title">Abilities<td>
+			<td class="table-data">${abilities.join(`<br>`)}</td>
+		</tr>
+	</table>
+	
+	`
+
+	bottomSheet.innerHTML = infoInnerHTML
+	document.querySelector('.fiche-container').appendChild(bottomSheet);
+
+		}
+			
+		)
+	}
+
+
+
+if (selectedPoke) {
+	fetchPokeData(selectedPoke)
+}
+else {
+	fetchPokemons()
+}
